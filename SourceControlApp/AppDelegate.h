d1ab952a281a1d365324480a7bcb593376e2ee35
jlcv <jlcv@github.com>
2016-01-26 12:15:34 -0600

    Initial Commit

---
+++ SourceControlApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SourceControlApp
+//
+//  Created by jChavez17 on 1/26/16.
+//  Copyright © 2016 jChavez17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SourceControlApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SourceControlApp
+//
+//  Created by jChavez17 on 1/26/16.
+//  Copyright © 2016 jChavez17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
